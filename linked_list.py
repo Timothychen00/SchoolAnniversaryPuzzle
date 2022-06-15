fe19d83a3d3e45cc79ea51ddfa92893e9bf0e9c4
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-06-15 17:22:46 +0800

    全物件導向

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,16 @@
+import os,random,re,pymongo
+from dotenv import load_dotenv
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage,ImageSendMessage,FollowEvent,UnfollowEvent,ButtonsTemplate,MessageTemplateAction,TemplateSendMessage,MessageAction,QuickReply,QuickReplyButton,Sender
+from flask import Flask,request,abort
+
+load_dotenv()
+client = pymongo.MongoClient("mongodb+srv://admin:"+os.environ['DB_PASSWORD']+"@cluster0.wvaw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.linebot
+collection=db.user
+
+
 class LinkedList():
     def __init__(self,msg):
         self.length=1
@@ -10,17 +23,20 @@ class LinkedList():
         self.tail=self.tail.next
         self.tail.next=None
 
-    def find(self,id):
+    def find_one(self,key,value):
         pointer=self.head
-        for _ in range(id):
+        length=self.length
+        for _ in range(length):
+            if value==pointer.__dict__[key]:
+                return pointer
             pointer=pointer.next
-        return pointer
+        return None
 
     def info(self):
-        print('head',self.head)
-        print('tail',self.tail)
-        print('length',self.length)
-        print('tree')
+        print('head:',self.head)
+        print('tail:',self.tail)
+        print('length:',self.length)
+        print('tree:')
         pointer=self.head
         for _ in range(self.length):
             pointer.info()
@@ -32,9 +48,11 @@ class Node():
         self.msg=msg
         self.quick_reply=quick_reply
         self.query=query
-        self.next=None
+        self.next=None#connection point不一定只有一個 可能會有很多個
         self.quick_reply=quick_reply
         self.sender=sender
+        self.degree=None
+
     def info(self):
         print('-'*20)
         print('id:',self.id)
@@ -44,12 +62,57 @@ class Node():
         print('query:',self.query)
         print('next:',self.next)
         
+class User():
+    client = pymongo.MongoClient("mongodb+srv://admin:"+os.environ['DB_PASSWORD']+"@cluster0.wvaw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    db = client.linebot
+    collection=db.user
+    
+    def __init__(self,userId):
+        self.user=self.collection.find_one({'type':'user'})[userId]
+        self.userId=userId
+        self.name=self.user[0][0]
+        self.branch=self.user[1][0]
+        self.position=self.user[1][1]
         
+    def info(self):
+        print('-'*20)
+        print('obj:',self)
+        print('name',self.name)
+        print('userId',self.userId)
+        print('branch',self.branch)
+        print('position',self.position)
         
+# class DB():
+#     def __init__(self):
+#         client = pymongo.MongoClient("mongodb+srv://admin:"+os.environ['DB_PASSWORD']+"@cluster0.wvaw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+#         db = self.client.linebot
+#         collection=self.db.user
+#         document=self.collection.find_one({'type':'user'})
+        
+    # def get_userdata
+
         
 __linkedlist=LinkedList('hh')
+
 __linkedlist.append('哈哈1','hh','智能助理')
 __linkedlist.append('哈哈2','hh','智能助理')
+
+
+__linkedlist.info()
+# 
+    
+    
+__linkedlist.find_one('query',None).info()
+
+print(type(__linkedlist))
+
+
+
+
+
+
+
+
 
 # __linkedlist.find(0).info()
 # __linkedlist.info()
@@ -57,7 +120,15 @@ __linkedlist.append('哈哈2','hh','智能助理')
 # __linkedlist.show_all()
 # __linkedlist.head.info()
 # __linkedlist.head.next.info()
-__linkedlist.head.next.next.info()
+# __linkedlist.head.next.next.info()
 # __linkedlist.info()
 
-    
+
+# __linkedlist.info()
+# __linkedlist.find(1).info()
+# __linkedlist.find(id,1).info()
+
+
+
+__user=User('Uf7f4867b3a62ac1bf05a26c54ddd3b2e')
+__user.info()
