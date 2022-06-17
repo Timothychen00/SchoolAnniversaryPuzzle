d9c7c143f9ad9572326fa2a788d18d6f1188d11f
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-06-17 13:06:00 +0800

    分支樹搜索完成

--- linked_list.py
+++ linked_list.py
@@ -11,13 +11,16 @@ client = pymongo.MongoClient("mongodb+srv://admin:"+os.environ['DB_PASSWORD']+"@
 db = client.linebot
 collection=db.user
 class LinkedList():
-    def __init__(self,msg):
+    def __init__(self,msg,name=None,id=0):
         self.length=1
-        self.head=Node(id=0,msg=msg)
+        self.name=name
+        self.head=Node(id=id,msg=msg,branch=self.name)
         self.tail=self.head
 
     def append(self,msg,quick_reply,sender):
-        self.tail.next.append(Node(id=self.length,msg=msg,quick_reply=quick_reply,sender=sender))
+        if not self.tail.next:
+            self.tail.next=[]
+        self.tail.next.append(Node(id=self.length,msg=msg,quick_reply=quick_reply,sender=sender,branch=self.name))
         self.length+=1
         self.tail=self.tail.next[0]
         self.tail.next=[]
@@ -25,7 +28,7 @@ class LinkedList():
     def insert_after(self,id,msg,quick_reply,sender):
         node=self.find_one(id)
         next=node.next
-        node.next=Node(id=self.length,msg=msg,quick_reply=quick_reply,sender=sender)
+        node.next=Node(id=self.length,msg=msg,quick_reply=quick_reply,sender=sender,branch=self.name)
         node.next.next=next
         self.length+=1
 
@@ -43,35 +46,36 @@ class LinkedList():
         print('tail:',self.tail)
         print('length:',self.length)
         print('tree:')
-        pointer=self.head
-        for _ in range(self.length):
-            if pointer:
-                for i in pointer:
-                    i.info()
-                pointer=pointer.next
-        print('\n'*5)
-        
-        
+        queue=[]
+        # print(self.head)
+        queue.append(self.head)
+        while queue:
+            print('current_queue:',queue)
+            pointer=queue[-1]
+            pointer.info()
+            queue.pop()
+            queue.extend(pointer.next)
         
     def switch(self):
         pass
 
 
 class Node():
-    def __init__(self,id=0,msg=None,quick_reply=None,query=None,next=None,sender=None):
+    def __init__(self,id=0,msg=None,quick_reply=None,query=None,next=None,sender=None,branch=None):
         self.id=id
         self.msg=msg
         self.quick_reply=quick_reply
         self.query=query#請求的內容
-        self.next=[]#connection point不一定只有一個 可能會有很多個
+        self.next=None#connection point不一定只有一個 可能會有很多個
         self.quick_reply=quick_reply
         self.sender=sender
         self.degree=None
         self.isVisit={}#userid
+        self.branch=branch
 
     def info(self):
         print('-'*20)
-        print('id:',self.id)
+        print('id:',self.branch,self.id)
         print('self:',self)
         print('msg:',self.msg)
         print('quick_reply',self.quick_reply)
@@ -79,13 +83,13 @@ class Node():
         print('next:',self.next)
         
     def connect(self,*nodes):
+        print('nextnext:::',self.next)
         if self.next:
-            next=self.next
-            self.next=[next]
             self.next.extend(nodes)
         else:
             self.next=[]
             self.next.extend(nodes)
+        print(self.next)
     
     def tag(self,user_id):
         self.isVisit.add(user_id)
@@ -106,7 +110,7 @@ class Node():
 #                 pass
 
         
-__linkedlist=LinkedList('hh')
+__linkedlist=LinkedList('hh','\033[94mbranch1\033[0m')
 
 __linkedlist.append('哈哈1','hh','智能助理')
 __linkedlist.append('哈哈2','hh','智能助理')
@@ -114,14 +118,14 @@ __linkedlist.append('哈哈2','hh','智能助理')
 
 __linkedlist.info()
 # 
-    
+print(__linkedlist.head)
     
 # __linkedlist.find_one('query',None).info()
 
 # print(type(__linkedlist))
-__linkedlist.insert_after(1,'哈哈哈3','hh','智能助理')
+# __linkedlist.insert_after(1,'哈哈哈3','hh','智能助理')
 
-__linkedlist.find_one(3).connect(4,Node('哈哈哈4'),'hh','智能助理')
+# __linkedlist.find_one(3).connect(4,Node('哈哈哈4'),'hh','智能助理')
 
 branches=[__linkedlist]
 class User():
@@ -150,9 +154,16 @@ class User():
 
 
 
+__linkedlist2=LinkedList('pp','\033[92mbranch2\033[0m')
+__linkedlist2.append('pp1','hh','智能助理')
+__linkedlist2.append('陳澤榮好帥','確實','智能助理')
+__linkedlist2.append('陳澤榮好帥2','確實','智能助理')
+__linkedlist2.append('陳澤榮好帥3','確實','智能助理')
 
-
-
+__linkedlist.head.next[0].connect(__linkedlist2.head)
+print('\n'*10)
+__linkedlist2.info()
+print('\n'*10)
 __linkedlist.info()
 
 
