8a459c51a96f24b16dc050a0c6596906423efe97
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-07-01 07:52:05 +0800

    final

--- app.py
+++ app.py
@@ -17,22 +17,30 @@ app=Flask(__name__)
 line_bot_api = LineBotApi(os.environ['access_token'])
 handler = WebhookHandler(os.environ['Channel_secret'])
 
-sender={x:Sender(name=x,icon_url=image_src[x]) for x in ['智能助理','阿豆','昱誠','黃準','旁白','觀眾','阿嘟','熱音社社員','司儀','吉他社社員','小傑']}
+sender={x:Sender(name=x,icon_url=image_src[x]) for x in list(image_src.keys())}
 
-def send(token,data,sender_name,reply=None):
+
+
+QuickReplyButton(action=MessageAction(label='1',text='1'))
+def send(token,data,sender_name,reply=None,username=None):
     print('==send==')
     msg=[]
     msg_length=len(data)
     msg_range=range(msg_length)
     label=reply
     if reply:
-        if len(reply)>6:
-            if '，' in reply:
-                label=reply.split('，')[0]
-            elif '！' in reply:
-                label=reply.split('！')[0]
-            elif '？' in reply:
-                label=reply.split('？')[0]
+        if not isinstance(reply, list):
+            if len(reply)>6:
+                if '，' in reply:
+                    label=reply.split('，')[0]
+                elif '！' in reply:
+                    label=reply.split('！')[0]
+                elif '？' in reply:
+                    label=reply.split('？')[0]
+            label=[label]
+            reply=[reply]
+        else:
+            pass
     print(sender_name)
     if not isinstance(sender_name, list):
         sender_name=[str(sender_name) for i in range(len(data))]
@@ -45,18 +53,40 @@ def send(token,data,sender_name,reply=None):
             print("-------------\n",data[i])
             if i==msg_length-1 and reply:
                 print(3)
-                msg.append(ImageSendMessage(original_content_url=image_src[data[i]],sender=sender[sender_name[i]],preview_image_url=preview_src[data[i]],quick_reply=QuickReply(items=[QuickReplyButton(action=MessageAction(label=label,text=reply))])))
+                actions=[]
+                for j in range(len(label)):
+                    actions.append(MessageAction(label=label[j],text=reply[j]))
+                if len(actions)==1:
+                    actions=actions[0]
+                print(actions)
+                msg.append(ImageSendMessage(original_content_url=image_src[data[i]],sender=sender[sender_name[i]],preview_image_url=preview_src[data[i]],quick_reply=QuickReply(items=[QuickReplyButton(action=actions)])))
             else:
                 print(4)
                 msg.append(ImageSendMessage(original_content_url=image_src[data[i]],sender=sender[sender_name[i]],preview_image_url=preview_src[data[i]]))
-        else:
+        else:# for x in label
+            temp_data=data[i]
+            if 'ID' in data[i]:
+                print('!!!!!!!')
+                temp_data=temp_data.replace('ID',username)
+                
             if i==msg_length-1 and reply:
                 print(1)
                 print(data[i])
-                msg.append(TextSendMessage(text=data[i],sender=sender[sender_name[i]],quick_reply=QuickReply(items=[QuickReplyButton(action=MessageAction(label=label,text=reply))])))
+                print(label)
+                actions=[]
+                if label[0]=='接通':
+                    actions=[MessageAction(label='接通',text='接通'),MessageAction(label='不接通',text='不接通')]
+                else:
+                    for j in range(len(label)):
+                        actions.append(MessageAction(label=label[j],text=reply[j]))
+                    if len(actions)==1:
+                        actions=actions[0]
+                    print('ac:',actions)
+                print(actions)
+                msg.append(TextSendMessage(text=temp_data,sender=sender[sender_name[i]],quick_reply=QuickReply(items=[QuickReplyButton(action=actions)])))
             else:
                 print(2)
-                msg.append(TextSendMessage(text=data[i],sender=sender[sender_name[i]]))
+                msg.append(TextSendMessage(text=temp_data,sender=sender[sender_name[i]]))
         print(token,len(msg))
         print(token,msg)
     line_bot_api.reply_message(token,msg)
@@ -72,25 +102,38 @@ def msg_process(event):
     
     user_set=collection.find_one({"type":'user1'})
     user=user_set[user_id]
+    username=user[0][0]
     user_data=user[1]
     print(user_data,'/////')
     branch=user_data[0]
     times=user_data[1]
     try_times=user_data[2]
     print(branch,times,try_times)
-    
+    print('username',username)
     if times==0 and re.match("開始遊戲",msg):
         times+=1
-
+    if 'back' in msg:
+        print(msg.split('back'))
+        print('1::',msg.split('back')[-1])
+        if msg.split('back')[-1]:
+            print(666)
+            print(int(msg.split('back')[-1]))
+            times-=(int(msg.split('back')[-1]))
+            collection.update({"type":'user1'},{"$set":{str(user_id):[[user[0][0]],[branch,times+1,try_times]]}})
+        else:
+            times-=1
+        send(token,msg_pack[branch][times][1],msg_pack[branch][times][0],msg_pack[branch][times][2])
+        return ''
+        
     if branch in question_pack and times in question_pack[branch]:
         # not (question_pack[branch][times] in msg)
         if type(question_pack[branch][times]) is list:
             if not (question_pack[branch][times][0][0] in msg):
                 if not (question_pack[branch][times][1][0] in msg):
                     if try_times<6:
-                        send(token,['text'],['好像不是欸'],msg_pack[branch][times][0],None)
+                        send(token,['好像不是欸'],msg_pack[branch][times][0],None)
                     else:
-                        send(token,['text'],['你是不是太笨了，需要幫助嗎'],msg_pack[branch][times][0],question_pack[branch][times])
+                        send(token,['你是不是太笨了，需要幫助嗎'],msg_pack[branch][times][0],question_pack[branch][times])
                     try_times+=1
                 else:
                     print('correct')
@@ -105,9 +148,9 @@ def msg_process(event):
         else:
             if not (question_pack[branch][times] in msg):
                 if try_times<6:
-                    send(token,['text'],['好像不是欸'],msg_pack[branch][times][0],None)
+                    send(token,['好像不是欸'],msg_pack[branch][times][0],None)
                 else:
-                    send(token,['text'],['你是不是太笨了，需要幫助嗎'],msg_pack[branch][times][0],question_pack[branch][times])
+                    send(token,['你是不是太笨了，需要幫助嗎'],msg_pack[branch][times][0],question_pack[branch][times])
                 try_times+=1
             else:
                 print('correct')
@@ -120,7 +163,12 @@ def msg_process(event):
         if msg_pack[branch][times-1][2]:
             print(re.match(msg_pack[branch][times-1][2],msg))
         if re.match(msg_pack[branch][times-1][2],msg):
-            send(token,msg_pack[branch][times][1],msg_pack[branch][times][0],msg_pack[branch][times][2])
+            # if 'ID' in msg_pack[branch][times][1]:
+            #     print('替換')
+            #     msg_pack[branch][times][1].replace('ID',username)
+            send(token,msg_pack[branch][times][1],msg_pack[branch][times][0],msg_pack[branch][times][2],username=username)
+            # if username in msg_pack[branch][times][1]:
+            #     msg_pack[branch][times][1].replace(username,'ID')
             times+=1
 
     collection.update({"type":'user1'},{"$set":{str(user_id):[[user[0][0]],[branch,times,try_times]]}})
